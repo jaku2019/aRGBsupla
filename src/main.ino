0a47caac1bb0583cba18e93a69c3eaa0343ca36a
jaku2019 <jakub.tatarski@gmail.com>
2024-12-31 22:11:40 +0100

    ArduinoOTA, gitignore update

--- src/main.ino
+++ src/main.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 /*
   Copyright (C) AC SOFTWARE SP. Z O.O.
 
@@ -393,6 +394,45 @@ void setup() {
   virtual_relay5->setInitialCaption("Fire2012 z paletą");
 
   SuplaDevice.begin();
+
+// Hostname domyślnie esp3232-[MAC]
+  ArduinoOTA.setHostname("choinka=aRGB");
+
+  // No authentication by default
+//  ArduinoOTA.setPassword("haslo!!!");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
 }
 
 void loop() {
@@ -495,7 +535,12 @@ void loop() {
 
   SuplaDevice.iterate();
 
-
+  if (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("OTA działa");
+    ArduinoOTA.begin();  // Start OTA
+  }
+  ArduinoOTA.handle();
 }
 
 
